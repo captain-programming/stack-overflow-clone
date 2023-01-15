99f4f5e9b3feaa081b3c7942da1f407967f7458b
Dinesh Karde <dineshkarde103@gmial.com>
2023-01-15 17:11:21 +0530

    add

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,13 +6,11 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className='home-container'>
-      <div className='home-container-1'>
-        {/* <LeftSidebar /> */}
-      </div>
+    <div className='home-container-1'>
+      <LeftSidebar />
       <div className='home-container-2'>
-        {/* <HomeMainbar />
-        <RightSidebar /> */}
+        <HomeMainbar />
+        <RightSidebar />
       </div>
     </div>
   )
